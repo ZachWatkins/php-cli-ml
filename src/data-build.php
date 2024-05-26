3163c07123815a16296e6d551d4761ea9591894f
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-05-25 22:35:55 -0500

    Add columns

--- src/data-build.php
+++ src/data-build.php
@@ -14,9 +14,9 @@ $filtered = [];
 
 for ($i = 0; $i < $length; $i++) {
     $poke = json_decode(file_get_contents("data/pokemon/{$pokemon[$i]['name']}.json"), true);
-    $species_data = $poke['species']['data'];
+    $species = $poke['species']['data'];
     $group = 'rows';
-    if ($poke['name'] !== $species_data['name']) {
+    if ($poke['name'] !== $species['name']) {
         $group = 'filtered';
     }
     array_push($$group, [
@@ -27,22 +27,25 @@ for ($i = 0; $i < $length; $i++) {
             return $group['name'];
         }, $poke['species']['data']['egg_groups'])) : '(no egg groups)'),
         (isset($species['growth_rate']) ? $species['growth_rate']['name'] : '(no growth rate)'),
-        $poke['species']['data']['color']['name'],
+        $species['color']['name'],
         $poke['height'],
         $poke['weight'],
-        (isset($poke['species']['data']['shape']['name']) ? $poke['species']['data']['shape']['name'] : '(no shape)')
+        (isset($species['shape']['name']) ? $species['shape']['name'] : '(no shape)'),
+        ($species['is_baby'] ? 'true' : 'false'),
+        ($species['is_legendary'] ? 'true' : 'false'),
+        ($species['is_mythical'] ? 'true' : 'false')
     ]);
 }
 
 $file = fopen('data/pokemon.csv', 'w');
-fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape']);
+fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape', 'is_baby', 'is_legendary', 'is_mythical']);
 foreach ($rows as $row) {
     fputcsv($file, $row);
 }
 fclose($file);
 
 $file = fopen('data/filtered.csv', 'w');
-fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape']);
+fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape', 'is_baby', 'is_legendary', 'is_mythical']);
 foreach ($filtered as $row) {
     fputcsv($file, $row);
 }
