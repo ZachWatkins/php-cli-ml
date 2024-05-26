ff0cf685261ca6a50333857a58746360fcea97f3
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2024-05-26 13:44:38 -0500

    Update the build file to include a training, testing, and complete CSV file

--- src/data-build.php
+++ src/data-build.php
@@ -1,4 +1,8 @@
 <?php
+const FILE_TRAINING_DATA = 'data/training.csv';
+const FILE_TESTING_DATA = 'data/testing.csv';
+const FILE_POKEMON_DATA = 'data/pokemon.csv';
+const HEADERS = ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape', 'is_baby', 'is_legendary', 'is_mythical'];
 
 /**
  * Extract a subset of Pokemon data to a CSV file.
@@ -9,44 +13,84 @@
 $pokemon = json_decode(file_get_contents('data/pokemon.json'), true);
 $length = count($pokemon);
 
-$rows = [];
-$filtered = [];
+function training(array $pokemon): bool
+{
+    return $pokemon['species']['data']['habitat'] ? true : false;
+}
+
+function testing(array $pokemon): bool
+{
+    $species_name = $pokemon['species']['name'];
+    if ($pokemon['name'] !== $species_name) {
+        if ($pokemon['name'] === "{$species_name}-normal") {
+            return true;
+        }
+        if ($pokemon['name'] === "{$species_name}-gmax") {
+            return false;
+        }
+        if ($pokemon['name'] === "{$species_name}-mega") {
+            return false;
+        }
+        if (!$pokemon['species']['data']['is_legendary'] && !$pokemon['species']['data']['is_mythical']) {
+            return true;
+        }
+        return false;
+    }
+    if (!$pokemon['order']) {
+        return false;
+    }
+    return true;
+}
+
+$training = [];
+$testing = [];
+$all = [];
 
 for ($i = 0; $i < $length; $i++) {
-    $poke = json_decode(file_get_contents("data/pokemon/{$pokemon[$i]['name']}.json"), true);
-    $species = $poke['species']['data'];
-    $group = 'rows';
-    if ($poke['name'] !== $species['name']) {
-        $group = 'filtered';
-    }
-    array_push($$group, [
-        $poke['species']['name'],
-        $poke['name'],
+    $mon = json_decode(file_get_contents("data/pokemon/{$pokemon[$i]['name']}.json"), true);
+    $species = $mon['species']['data'];
+    $values = [
+        $mon['species']['name'],
+        $mon['name'],
         (isset($species['habitat']) ? $species['habitat']['name'] : '(no habitat)'),
         (isset($species['egg_groups']) ? implode(',', array_map(function ($group) {
             return $group['name'];
-        }, $poke['species']['data']['egg_groups'])) : '(no egg groups)'),
+        }, $mon['species']['data']['egg_groups'])) : '(no egg groups)'),
         (isset($species['growth_rate']) ? $species['growth_rate']['name'] : '(no growth rate)'),
         $species['color']['name'],
-        $poke['height'],
-        $poke['weight'],
+        $mon['height'],
+        $mon['weight'],
         (isset($species['shape']['name']) ? $species['shape']['name'] : '(no shape)'),
         ($species['is_baby'] ? 'true' : 'false'),
         ($species['is_legendary'] ? 'true' : 'false'),
         ($species['is_mythical'] ? 'true' : 'false')
-    ]);
+    ];
+    $all[] = $values;
+    if (training($mon)) {
+        $training[] = $values;
+    }
+    if (testing($mon)) {
+        $testing[] = $values;
+    }
 }
 
-$file = fopen('data/pokemon.csv', 'w');
-fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape', 'is_baby', 'is_legendary', 'is_mythical']);
-foreach ($rows as $row) {
+$file = fopen(FILE_POKEMON_DATA, 'w');
+fputcsv($file, HEADERS);
+foreach ($all as $row) {
     fputcsv($file, $row);
 }
 fclose($file);
 
-$file = fopen('data/filtered.csv', 'w');
-fputcsv($file, ['species', 'name', 'habitat', 'egg_groups', 'growth_rate', 'color', 'height', 'weight', 'shape', 'is_baby', 'is_legendary', 'is_mythical']);
-foreach ($filtered as $row) {
+$file = fopen(FILE_TRAINING_DATA, 'w');
+fputcsv($file, HEADERS);
+foreach ($training as $row) {
+    fputcsv($file, $row);
+}
+fclose($file);
+
+$file = fopen(FILE_TESTING_DATA, 'w');
+fputcsv($file, HEADERS);
+foreach ($testing as $row) {
     fputcsv($file, $row);
 }
 fclose($file);
